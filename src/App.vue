3ec8255f0af392fd006683d9179ccab7f0961d81
achunin <aleksandr.chunin@t-systems.com>
2022-01-02 18:05:21 +0300

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <RegisterForm msg=" "/>
+
+  <router-view/>
 </template>
 
-<script>
-import RegisterForm from "@/components/RegisterForm";
-
-export default {
-  name: 'App',
-  components: {
-    RegisterForm
-  }
-}
-</script>
-
 <style>
-@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
+
+
 </style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,54 @@
+<template>
+
+
+  <h1>User Alice</h1>
+  <div class="container">
+    <div class="row" >
+      Name: Alice
+    </div>
+    <div class="row">
+      E-Mail: alice@mail.df
+    </div>
+    <div class="row">
+      Phone: +932423434324
+    </div>
+    <div class="row">
+      Messengers: Telegram
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  props: {
+    msg: String
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+body {
+  height: 100%;
+}
+
+.h-100 {
+  height: 100% !important;
+}
+
+main {
+  min-height: 30%;
+}
+</style>
