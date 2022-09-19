8190441a078346743d447fddd31035fd2dd6bd4b
onlythenaive <ilya.gubarev@gmail.com>
2022-09-20 02:19:12 +0300

    Login/auth interceptor added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,24 @@
   <router-view/>
 </template>
 
+<script>
+
+  import axios from "axios";
+
+  export default {
+
+    created: function () {
+      axios.interceptors.request.use(config => {
+        let token = localStorage.getItem("accessToken");
+        if (token) {
+          config.headers["Authorization"] = "Bearer " + token;
+        }
+        return config;
+      }, error => Promise.reject(error));
+    }
+  };
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
