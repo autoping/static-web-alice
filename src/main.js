11de9a41f761a024b58d320fe73eae71d02ac838
achunin <aleksandr.chunin@t-systems.com>
2023-08-17 20:20:19 +0300

    scss and add main color and font

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bulma/css/bulma.css'
+import './assets/styles/main.scss'
+// import 'bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App)
